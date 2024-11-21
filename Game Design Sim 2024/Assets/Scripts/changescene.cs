33d91cee612e6d7800f3218e612a0cc04b8176d5
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-20 20:44:55 -0500

    Updates

--- Game Design Sim 2024/Assets/Scripts/changescene.cs
+++ Game Design Sim 2024/Assets/Scripts/changescene.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement; 
 
 public class changescene : MonoBehaviour
 {
     public GameObject credits;
+    public GameObject playButton;
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(playButton);
     }
 
     // Update is called once per frame
